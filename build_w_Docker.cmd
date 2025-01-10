1a738523f0138e008dfba984c850c919951a6d89
Franco Polo <franco.joel.polo@iis.fraunhofer.de>
2025-01-10 11:41:49 +0100

    Fix cmd script comments

--- build_w_Docker.cmd
+++ build_w_Docker.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-@REM Pull the Docker image
+:: Pull the Docker image
 docker pull fjpolo/fpgatools_ubuntu22.04_oss-cad-suite_fv:202501061639
 
-@REM Run the Docker container and execute the specified commands
+:: Run the Docker container and execute the specified commands
 docker run -v %cd%:/workspace -it fjpolo/fpgatools_ubuntu22.04_oss-cad-suite_fv:202501061639 /bin/bash /workspace/scripts/build_sdram_fs.sh
