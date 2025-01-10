190d7e1007bd76fd5811739b58999e9bba8061fa
Franco Polo <franco.joel.polo@iis.fraunhofer.de>
2025-01-10 11:31:26 +0100

    Fix build scripts

--- build_w_Docker.cmd
+++ build_w_Docker.cmd
@@ -4,4 +4,4 @@
 docker pull fjpolo/fpgatools_ubuntu22.04_oss-cad-suite_fv:202501061639
 
 @REM Run the Docker container and execute the specified commands
-docker run -v %cd%:/workspace -it fjpolo/fpgatools_ubuntu22.04_oss-cad-suite_fv:202501061639 -c "source /oss-cad-suite/environment;cd /workspace/src;make sdram.fs"
+docker run -v %cd%:/workspace -it fjpolo/fpgatools_ubuntu22.04_oss-cad-suite_fv:202501061639 /bin/bash /workspace/scripts/build_sdram_fs.sh
